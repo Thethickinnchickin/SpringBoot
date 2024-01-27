0339a825e36e02aca6fbc684f3922f2c8fc5e3b2
Thethickinnchickin <matt.reiley@sjsu.edu>
2024-01-26 17:14:27 -0800

    version .1

--- src/main/java/org/springboardLogin/Security/SecurityConfig.java
+++ src/main/java/org/springboardLogin/Security/SecurityConfig.java
@@ -11,11 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
 
 @Configuration
 @EnableWebSecurity
